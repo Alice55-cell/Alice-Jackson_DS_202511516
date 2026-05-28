3294eefa427cef928dfe9db5358c48474be66c3c
Alice55-cell <aj5699974@gmail.com>
2026-05-28 23:13:34 +0300

    Add files via upload

---
+++ notepad.h
@@ -0,0 +1,10 @@
+#pragma 
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+#include <string>
+
+void writeFile(std::string filename);
+void readFile(std::string filename);
+void appendFile(std::string filename);
+
+#endif
